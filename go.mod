04036afd7f05e5b9acbfcc5df019c5712d2b7ad3
SaurabPoudel <poudelsaurab20@gmail.com>
2024-11-21 22:04:56 +0545

    text search from scratch

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SaurabPoudel/full-text-search-engine
+
+go 1.23.2
---
+++ utils/filter.go
@@ -0,0 +1,22 @@
+package utils
+
+import "strings"
+
+func lowercaseFilter(tokens []string) []string{
+	r := make([]string, len(tokens))
+	for i, token := range tokens {
+		r[i] = strings.ToLower(token)
+	}
+	return r
+}
+
+func stopwordFilter(tokens []string) []string {
+	var stopwords = map[string] struct{}{
+		"a": {}, "and": {}, "be": {}, "have": {}, "i": {},
+		"in": {}, "of": {}, "that": {}, "the": {}, "to": {},
+	}
+	r := make([]string, 0, len(tokens))
+	for _, token 
+}
+
+func stemmerFilter
